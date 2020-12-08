a9a81046ccb9e2bb9e6a183de1eeafd5b9cbb72b
xiaozhublog <350122713@qq.com>
2020-12-08 22:55:36 +0800

    初始化项目，安装ElementUI,reset样式，删除默认页面

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # admin-element-template
 本项目Vue+element实现后台管理系统
+
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <div>
+      <el-button type="success">Xiaozhublog</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {}
+};
+</script>
